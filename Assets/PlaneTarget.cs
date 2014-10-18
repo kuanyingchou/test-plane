7de07ef670798a9c2045d0792e268d49291315f7
ken <kuanyingchou@gmail.com>
2014-10-18 23:09:10 +0800

    fix plane slow-down bug; add axis control

--- Assets/PlaneTarget.cs
+++ Assets/PlaneTarget.cs
@@ -2,15 +2,19 @@
 using System.Collections;
 
 public class PlaneTarget : MonoBehaviour {
-    public Vector2 v = new Vector2(50, 50);
+    public Vector2 v = new Vector2(1, 1);
+    public float s = 1;
     private float rx, ry;
-    //private Vector3 oldPos = Vector3.zero;
     public Transform plane;
-
-    public void Start() { }
+    public float earthRadius = 50;
 
     public void Update()
     {
+
+        v = new Vector2(v.x + -Input.GetAxis("Horizontal") * s,
+                        v.y + Input.GetAxis("Vertical") * s);
+
+
         Transform coreTransform = transform.parent;
         float x = coreTransform.eulerAngles.x;
         float a = 60;
@@ -20,11 +24,25 @@ public class PlaneTarget : MonoBehaviour {
             v = new Vector2(v.x, -v.y);
         }
 
-        rx += v.y * Time.deltaTime;
-        ry += v.x * Time.deltaTime;
+        float longitude = x;
+        float radiusAtLongitude = 
+                earthRadius * Mathf.Cos(Mathf.Abs(
+                longitude * Mathf.Deg2Rad));
+        float circumferenceAtLongitude = 
+                2 * radiusAtLongitude * Mathf.PI;
+        float HorizontalDistancePerDegree = 
+                circumferenceAtLongitude / 360f;
+        //Debug.Log(string.Format("longitude: {0}, current radius: {1}", 
+        //    longitude, radiusAtLongitude));
+        float circumference = 2 * earthRadius * Mathf.PI;
+        float VerticalDistancePerDegree = circumference / 360f;
+
+        rx += v.y / VerticalDistancePerDegree * Time.deltaTime;
+        ry += v.x / HorizontalDistancePerDegree *  
+                Time.deltaTime;
         coreTransform.rotation = Quaternion.Euler(rx, ry, 0);
         UpdatePlane();
-        DebugRay(coreTransform);
+        //DebugRay(coreTransform);
     }
 
     private void DebugRay(Transform coreTransform)
